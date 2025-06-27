658715f1590f729a6bc8d12c4192e69fc52cba3b
Northern-spirit <btld_grekhov@mail.ru>
2025-06-27 13:44:04 +0500

    add new design for favorites

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -30,51 +30,61 @@ export default function FavoritesScreen() {
   };
 
   const renderItem = ({ item }: { item: any }) => (
-    <TouchableOpacity 
-      style={styles.card}
+    <TouchableOpacity
       onPress={() => router.push(`/car/${item.id}`)}
+      style={styles.card}
     >
       <View style={styles.imageContainer}>
         <Image
           source={{ uri: item.image }}
           style={styles.image}
         />
-        <TouchableOpacity 
-          style={styles.heartButton}
-          onPress={(e) => {
-            e.stopPropagation();
-            removeFromFavorites(item.id);
-          }}
-        >
-          <Ionicons name="heart" size={24} color="#FF3B30" />
-        </TouchableOpacity>
+
       </View>
-      <Text style={styles.title}>{item.title}</Text>
-      <Text style={styles.price}>{item.priceDisplay}</Text>
-      
-      <View style={styles.characteristics}>
-        <Text style={styles.characteristicText}>
-          {item.age} лет • {item.acceleration}с до 100км/ч • {item.horsepower} л.с.
-        </Text>
-      </View>
-      
-      <View style={styles.footer}>
-        <View style={styles.rating}>
-          <Image 
-            source={getRatingImage(item.rating)} 
-            style={styles.ratingImage}
-            resizeMode="contain"
-          />
-          <Text style={styles.ratingText}>{item.rating}</Text>
-          <Text style={styles.reviewsCount}>({item.reviewsCount})</Text>
+      <View style={styles.infoContainer}>
+        <Text numberOfLines={1} ellipsizeMode="tail" style={styles.title}>{item.title}</Text>
+        <Text style={styles.price}>{item.priceDisplay}</Text>
+
+        {/* <View style={styles.characteristics}>
+          <Text style={styles.characteristicText}>
+            {item.age} лет • {item.acceleration}с до 100км/ч • {item.horsepower} л.с.
+          </Text>
+        </View> */}
+
+        <View style={styles.footer}>
+          <View style={styles.rating}>
+            <Text style={styles.ratingText}>{item.rating}</Text>
+            <Image
+              source={getRatingImage(item.rating)}
+              style={styles.ratingImage}
+              resizeMode="contain"
+            />
+          </View>
+          <Text style={styles.distance}>{item.distance}</Text>
         </View>
-        <Text style={styles.distance}>{item.distance}</Text>
+        <View style={styles.buttonContainerWrapper}>
+          <TouchableOpacity style={styles.buttonContainer} onPress={() => router.push(`/car/${item.id}`)}>
+            <Text style={styles.buttonText}>Забронироватьы</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.heartButton}
+            onPress={(e) => {
+              e.stopPropagation();
+              removeFromFavorites(item.id);
+            }}
+          >
+            <Ionicons name="heart" size={24} color="#FF3B30" />
+          </TouchableOpacity>
+        </View>
       </View>
+
     </TouchableOpacity>
   );
 
   return (
     <View style={styles.container}>
+      <Text style={styles.titlePage}>Избранное</Text>
+      <View>Фильтры</View>
       <FlatList
         data={favorites}
         renderItem={renderItem}
@@ -96,6 +106,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
+  titlePage: {
+    fontSize: 36,
+    fontWeight: '700',
+    marginHorizontal: 20,
+    marginTop: 14,
+  },
   emptyText: {
     marginTop: 16,
     fontSize: 16,
@@ -105,62 +121,73 @@ const styles = StyleSheet.create({
   list: {
     padding: 16,
   },
+  infoContainer: {
+    flex: 1,
+    overflow: 'hidden',
+  },
   card: {
-    backgroundColor: '#fff',
-    borderRadius: 12,
     marginBottom: 16,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
+    flexDirection: 'row',
+  },
+  buttonContainerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: 101,
+    height: 24,
+    backgroundColor: '#9980FF',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 12,
+  },
+  buttonText: {
+    fontSize: 10,
+    fontWeight: '600',
+    color: 'white',
   },
   imageContainer: {
     position: 'relative',
   },
   image: {
-    width: '100%',
-    height: 200,
-    borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
+    width: 181,
+    height: 130,
+    borderRadius: 20,
   },
   heartButton: {
-    position: 'absolute',
-    top: 12,
-    right: 12,
     backgroundColor: 'rgba(255, 255, 255, 0.9)',
     borderRadius: 20,
-    padding: 8,
   },
   title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 12,
-    marginHorizontal: 16,
+    fontSize: 16,
+    fontWeight: '400',
+    marginTop: 11,
+    marginHorizontal: 12,
+    maxWidth: 181,
   },
   price: {
-    fontSize: 16,
-    color: '#007AFF',
+    fontSize: 14,
+    color: 'black',
+    fontWeight: '800',
     marginTop: 4,
-    marginHorizontal: 16,
+    marginHorizontal: 12,
   },
   footer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: 'column',
     marginTop: 8,
-    marginBottom: 16,
-    marginHorizontal: 16,
+    marginBottom: 6,
+    marginHorizontal: 12,
   },
   rating: {
+    gap: 4,
     flexDirection: 'row',
     alignItems: 'center',
   },
   ratingText: {
-    marginLeft: 4,
     fontSize: 14,
     color: '#666',
   },
